4fc6f43bf9abc025b0fd0cc78660627ddb8f41e0
Rafasuar <suarez.rafael.a@outlook.com>
2022-11-29 21:25:41 -0800

    changed spacing

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -8,7 +8,7 @@ function RecipeCard({strMeal, strMealThumb, strInstructions }) {
       <Card style={{ width: '14rem', margin: '10px', backgroundColor:'#8085a8', border:'solid'}}>
         <Card.Img variant="top" src={strMealThumb} />
         <Card.Body className='d-inline p-1 m-2 '>
-          <Card.Title className='d-inline-block m-2 p-1 text-light' style={{position:'relative', fontSize:'16px', right:'-15px'}}>{strMeal}</Card.Title>
+          <Card.Title className='d-inline-block m-2 p-2 text-light' style={{position:'relative', fontSize:'16px', right:'-15px'}}>{strMeal}</Card.Title>
           <RecipeModal title={strMeal} description={strInstructions} />
         </Card.Body>
       </Card>
